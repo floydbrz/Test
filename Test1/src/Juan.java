debc7aaf21f5a6e1cf457c740f89065ce3f4a77a
floydbrz <122246367+floydbrz@users.noreply.github.com>
2023-02-20 00:47:30 +0530

    Update Juan.java

--- Test1/src/Juan.java
+++ Test1/src/Juan.java
@@ -3,7 +3,7 @@ public class Juan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hey World");
+        System.out.println("Hey World 12");
 	}
 
 }
